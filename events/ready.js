9ededa31ec1362a7b2019455ad25239fa3e550d4
ShriAmbhikesh <126344921+ShriAmbhikesh@users.noreply.github.com>
2023-05-13 21:01:07 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -5,17 +5,13 @@ module.exports = {
   once: true,
   async execute(client) {
     console.log(`Ready! Logged in as ${client.user.tag}`);
-
-    // Replace THREAD_CHANNEL_ID with the actual ID of the thread channel you want to send the message to
     const threadChannelId = '1106250746095214765';
 
     try {
-      // Fetch the thread channel object using the threadChannelId
       const threadChannel = await client.channels.fetch(threadChannelId);
       
-      // Check if the channel is a thread channel
+     
       if (threadChannel.isThread()) {
-        // Send the message in the thread channel
         const message = await threadChannel.send('Bot is now online in this thread!');
         console.log(`Message sent in thread ${threadChannel.name}: ${message.content}`);
       } else {
